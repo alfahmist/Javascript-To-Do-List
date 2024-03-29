98902d09078af13cdb829ba5e71b2694b950080c
alfahmist <gst.alfahmi@gmail.com>
2024-03-29 16:40:05 +0700

    change P to Input

--- script.js
+++ script.js
@@ -4,24 +4,39 @@ const containerList = document.querySelector("#container-list");
 let list = document.getElementsByClassName("list");
 
 const add = () => {
+	// if no text dont run this if
 	if (textInput.value != "") {
+		// Create list
 		const createDiv = document.createElement("div");
-		const createText = document.createElement("p");
-		const createRemove = document.createElement("span");
 		createDiv.className = "list";
-		createRemove.classList = "remove";
+
+		// Create input .textEdit
+		const createText = document.createElement("input");
+		createText.value = textInput.value;
+		createText.className = "textEdit";
+
+		// Remove Icon
+		const createRemove = document.createElement("span");
+		createRemove.className = "remove";
 		createRemove.setAttribute("onclick", "removeThis(this)");
-		createText.innerHTML = textInput.value;
 		createRemove.innerHTML = "X";
+
+		// Append createText and createRemove to CreateDiv
 		createDiv.append(createText, createRemove);
+
+		// Append createDiv to containerList
 		containerList.appendChild(createDiv);
-		// textInput.value = "";
+
+		// Text reset
+		textInput.value = "";
 	}
 };
 
 const removeButton = () => {
+	// remove last list
 	list[list.length - 1].remove();
 };
 const removeThis = (el) => {
+	// remove thist element
 	el.parentElement.remove();
 };
